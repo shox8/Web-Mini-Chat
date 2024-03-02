206ff487eb6e3258d81d0110dc379842c4e1b86b
Shoxruh <codershoxruh@gmail.com>
2024-03-03 00:12:48 +0500

    Optimized

--- index.js
+++ index.js
@@ -12,8 +12,18 @@ const {
   doc,
 } = require("firebase/firestore");
 
-app.use(express.static("public"));
 app.use(express.json());
+
+const sf = (res, filePath) => res.sendFile(__dirname + "/public/" + filePath);
+
+app.get("/", (_, res) => sf(res, "index.html"));
+app.get('/style.css', (_, res) => sf(res, "style.css"));
+app.get('/main.js', (_, res) => sf(res, "main.js"));
+app.get('/assets/logo.png', (_, res) => sf(res, "assets/logo.png"));
+app.get('/assets/bg.gif', (_, res) => sf(res, "assets/bg.gif"));
+app.get('/libs/socket.io.js', (_, res) => sf(res, "libs/socket.io.js"));
+app.get('/libs/moment.js', (_, res) => sf(res, "libs/moment.js"));
+app.get('/libs/tailwind-all.css', (_, res) => sf(res, "libs/tailwind-all.css"));
 
 io.on("connection", (socket) => {
   socket.on("getMessages", () => {
